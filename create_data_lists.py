36f360c41cf42fcf0d8175b4a94be8924d8619e6
AntoineMath <antoine.mathu@gmail.com>
2019-12-06 16:32:29 +0100

    changed to another directory containing 1k images

--- create_data_lists.py
+++ create_data_lists.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
 #    create_data_lists_from_voc(voc07_path='/Users/mathurin/ml/PRuDENCE/SSD/VOC2007',
 #                      voc12_path='/Users/mathurin/ml/PRuDENCE/SSD/VOC2012',
 #                      output_folder='./')
-    create_data_lists('/home/mathurin/prudence/data/', '.')
+    create_data_lists('/home/mathurin/prudence/Image_04_12_19/', '.')
