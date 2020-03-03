7a9d9c929a38a5e939e2e8171edfd439386c6f70
AntoineMath <antoine.mathu@gmail.com>
2020-03-03 09:54:25 +0100

    now standardize eval images using mean and std of the training set

--- eval.py
+++ eval.py
@@ -19,7 +19,8 @@ args = parser.parse_args()
 pp = PrettyPrinter()
 
 # Parameters
-mean_std = [27970.5, 188.4542]
+training_set = ThermalDataset(".", split='train')
+mean, std = training_set.dataset_mean, training_set.dataset_std
 data_folder = args.test_folder
 keep_difficult = True  # difficult ground truth objects must always be considered in mAP calculation, because these objects DO exist!
 batch_size = 1
@@ -36,7 +37,7 @@ model = model.to(device)
 model.eval()
 
 # Load test data
-test_dataset = ThermalDataset(args.test_folder, split='test', mean_std=mean_std, keep_difficult=keep_difficult)
+test_dataset = ThermalDataset(args.test_folder, split='test', mean_std=[mean, std], keep_difficult=keep_difficult)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
                                           collate_fn=test_dataset.collate_fn, num_workers=workers, pin_memory=True)
 
